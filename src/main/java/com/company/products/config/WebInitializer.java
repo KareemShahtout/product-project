c395b64f5075f7df05531a15ebc6b9a14f0c0afd
Abdulkareem <abdulkareemshahtout@gmail.com>
2026-04-05 19:01:29 +0300

    setup App and Spring MVC and Hibernate base configuration

---
+++ src/main/java/com/company/products/config/WebInitializer.java
@@ -0,0 +1,20 @@
+package com.company.products.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{AppConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{MvcConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
