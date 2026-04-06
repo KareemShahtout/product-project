88f62a0790122b4fc20e11fcbae0eed47f0c680f
Abdulkareem <abdulkareemshahtout@gmail.com>
2026-04-06 21:41:51 +0300

    refactor(config): separate root and mvc component scanning

--- src/main/java/com/company/products/config/AppConfig.java
+++ src/main/java/com/company/products/config/AppConfig.java
@@ -2,8 +2,10 @@ package com.company.products.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 
 @Configuration
-@ComponentScan(basePackages = "com.company.products")
+@ComponentScan(basePackages = "com.company.products",
+        excludeFilters = @ComponentScan.Filter(Controller.class))
 public class AppConfig {
 }
--- src/main/java/com/company/products/config/MvcConfig.java
+++ src/main/java/com/company/products/config/MvcConfig.java
@@ -1,12 +1,14 @@
 package com.company.products.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
 @Configuration
+@ComponentScan(basePackages = "com.company.products.controller")
 public class MvcConfig {
     @Bean
     public InternalResourceViewResolver viewResolver() {
