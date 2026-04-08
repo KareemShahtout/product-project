6685589adbad0d3d9cce64ab999aae5926eb9286
Abdulkareem <abdulkareemshahtout@gmail.com>
2026-04-08 14:38:31 +0300

    rename ProductDao to ProductDaoHibernateImpl and add ProductDao interface

--- src/main/java/com/company/products/dao/ProductDao.java
+++ src/main/java/com/company/products/dao/ProductDao.java
@@ -1,61 +1,19 @@
 package com.company.products.dao;
 
 import com.company.products.entity.Product;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 
-import javax.annotation.PostConstruct;
 import java.util.List;
 
-@Repository
-public class ProductDao {
+public interface ProductDao {
 
-    @Autowired
-    private SessionFactory sessionFactory;
+    void save(Product product);
 
-    @PostConstruct
-    public void validateConfig() {
-        if (sessionFactory == null) {
-            throw new RuntimeException("SessionFactory not injected");
-        }
-    }
+    void deleteById(int id);
 
-    public void save(Product product) {
-        sessionFactory.getCurrentSession().persist(product);
-    }
+    void update(Product product);
 
-    public void deleteById(int id) {
-//        sessionFactory.getCurrentSession().createQuery("delete from ProductDetails where product_id = :id")
-//                .setParameter("id", id)
-//                .executeUpdate();
-//
-//        sessionFactory.getCurrentSession().createQuery("delete from Product where id = :id")
-//                .setParameter("id", id)
-//                .executeUpdate();
+    Product findById(int id);
 
-        Product product = findById(id);
-        if (product != null) {
-            sessionFactory.getCurrentSession().delete(product);
-        }
-
-    }
-
-    public void update(Product product) {
-        sessionFactory.getCurrentSession().merge(product);
-//      Session session = sessionFactory.getCurrentSession();
-//      session.update(product);
-    }
-
-    public Product findById(int id) {
-        Session session = sessionFactory.getCurrentSession();
-        return session.get(Product.class, id);
-    }
-
-    public List<Product> findAll() {
-        Session session = sessionFactory.getCurrentSession();
-        return session.createQuery("from Product", Product.class).list();
-    }
+    List<Product> findAll();
 
 }
---
+++ src/main/java/com/company/products/dao/ProductDaoHibernateImpl.java
@@ -0,0 +1,66 @@
+package com.company.products.dao;
+
+import com.company.products.entity.Product;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Repository
+public class ProductDaoHibernateImpl implements ProductDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @PostConstruct
+    public void validateConfig() {
+        if (sessionFactory == null) {
+            throw new RuntimeException("SessionFactory not injected");
+        }
+    }
+
+    @Override
+    public void save(Product product) {
+        sessionFactory.getCurrentSession().persist(product);
+    }
+
+    @Override
+    public void deleteById(int id) {
+//        sessionFactory.getCurrentSession().createQuery("delete from ProductDetails where product_id = :id")
+//                .setParameter("id", id)
+//                .executeUpdate();
+//
+//        sessionFactory.getCurrentSession().createQuery("delete from Product where id = :id")
+//                .setParameter("id", id)
+//                .executeUpdate();
+
+        Product product = findById(id);
+        if (product != null) {
+            sessionFactory.getCurrentSession().delete(product);
+        }
+
+    }
+
+    @Override
+    public void update(Product product) {
+        sessionFactory.getCurrentSession().merge(product);
+//      Session session = sessionFactory.getCurrentSession();
+//      session.update(product);
+    }
+
+    @Override
+    public Product findById(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(Product.class, id);
+    }
+
+    @Override
+    public List<Product> findAll() {
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("from Product", Product.class).list();
+    }
+
+}
